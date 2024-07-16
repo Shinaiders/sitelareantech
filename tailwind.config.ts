3f9daf1795be048c159ff9778de97d8dfe9db38e
Otavio <aotavio061@gmail.com>
2024-07-16 09:53:29 -0300

    feat: add new elements and new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        epilogue: ["Epilogue", "sans-serif"], // Substitua 'YourFontName' pelo nome da sua fonte
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
