bf3864f9562da500df147a93368b5249174b4a18
Lucas Santana <lucasadd15@gmail.com>
2024-07-23 18:56:20 -0300

    alterando codigo

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16651903030"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16651903030');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.className} flex flex-col relative`}>
         <div>
           <NavBar />
