f31bd20c2117e12f0c83b9ad8a31351e57c850b7
Lucas Santana <lucasadd15@gmail.com>
2024-08-01 18:38:55 -0300

    Update layout.tsx
    add google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,25 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
-      <head>
-      <meta name="google-adsense-platform-account" content="ca-host-pub-4369739311457036" />
-        {/* Google tag (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16651903030"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-16651903030');
-            `,
-          }}
-        />
-        
+    <html lang="pt-BR">      
+      <head>      
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MS555QG7');</script>
+                  
       </head>
       <body className={`${inter.className} flex flex-col relative`}>
         <div>
