1952e0935e3132414f8413810eb2034094cfd582
Otavio <aotavio061@gmail.com>
2024-08-03 19:13:16 -0300

    feat: add Project alert

--- app/(landingpage)/projects/page.tsx
+++ app/(landingpage)/projects/page.tsx
@@ -1,7 +1,34 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/_components/ui/card";
+import { Epilogue } from "next/font/google";
+const epilogue = Epilogue({ subsets: ["latin"] });
+
 export default function Projects() {
   return (
-    <div className="w-full h-full xl:px-20 px-2 space-y-40 font-epilogue mb-40">
-      <h1 className="text-4xl font-bold">Projects</h1>
+    <div
+      className={` ${epilogue.className} flex items-center justify-center w-full h-full xl:px-20 px-2 space-y-40 font-epilogue mb-40`}
+    >
+      <div className="w-96 h-[26rem] mt-40">
+        <Card>
+          <CardHeader>
+            <CardTitle>LareanTech</CardTitle>
+            <CardDescription>Seja bem vindo ao LareanTech</CardDescription>
+          </CardHeader>
+          <CardContent className="w-full ">
+            <p>
+              Nossa equipe está empenhada a preparar o portfolio da LareanTech.
+              Estamos a trabalhar em vários projetos, e esperamos que você seja
+              bem vindo.
+            </p>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
